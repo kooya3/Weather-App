4dcf7b45eb86447c79aca0bf813912a3dc5d34bb
Elyees Tatua <elyeestatua@gmail.com>
2024-02-19 18:50:08 +0300

    chore(imports): Adding imports to codebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+
 export default function Home() {
   return (
     <main className="text-6xl">
+      <Card>
       <h1>Let's build a GPT-4 Weather App</h1>
+      </Card>
     </main>
   );
 }
