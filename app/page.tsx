6086c748c46fe95cefe1c53a1b6968f3186551aa
Elyees Tatua <elyeestatua@gmail.com>
2024-02-19 18:52:11 +0300

    chore(page): working on the text field

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <main className="text-6xl">
       <Card>
+        <Text>Weather AI</Text>
       <h1>Let's build a GPT-4 Weather App</h1>
       </Card>
     </main>
