dfac76a48f14b2a2df68a7c7343cfeccc7d1fa67
Elyees Tatua <elyeestatua@gmail.com>
2024-02-19 19:49:50 +0300

    ops(page): Adding classes to the codebase

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="text-6xl">
       <Card>
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
-        <Subtitle>
+        <Subtitle className="text-xl text-center">
           Powered by OpenAI, Next.js 13.2, Tailwind CSS, Tremor 2.0 + More!
         </Subtitle>
       <h1>Let's build a GPT-4 Weather App</h1>
