419a0c1a26797abcc5cd741c3a79f48b22fc1cdb
Elyees Tatua <elyeestatua@gmail.com>
2024-02-19 19:19:52 +0300

    chore(page): Populating the subtitle field

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
     <main className="text-6xl">
       <Card>
         <Text>Weather AI</Text>
+        <Subtitle>
+          Powered by OpenAI, Next.js 13.2, Tailwind CSS, Tremor 2.0 + More!
+        </Subtitle>
       <h1>Let's build a GPT-4 Weather App</h1>
       </Card>
     </main>
