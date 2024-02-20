356e82f506e2b579728723f2820f1fc756feeeb5
Elyees Tatua <elyeestatua@gmail.com>
2024-02-20 23:54:15 +0300

    chore(page): redefining classnames for div fn

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
       <Card>
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">
