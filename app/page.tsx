c154d53e0c3e5c4c578948067913eaa846f7b680
Elyees Tatua <elyeestatua@gmail.com>
2024-02-20 23:55:26 +0300

    chore(page): redefining classnames for card

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
-      <Card>
+    <div className="min-h-screen  p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">
           Powered by OpenAI, Next.js 13.2, Tailwind CSS, Tremor 2.0 + More!
