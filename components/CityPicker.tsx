46a677d8689abaebfbb1a036b2c67a3a2d1ff48a
Elyees Tatua <elyeestatua@gmail.com>
2024-02-21 21:27:44 +0300

    chore(components): creating select options functionality

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -4,8 +4,13 @@ import { Country, City } from 'country-state-city';}
 import select from "react-select";
 
 const options = Country.getAllCountries().map(country => ({
-  
-}))
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}));
 
 function CityPicker = () => {
   return (
