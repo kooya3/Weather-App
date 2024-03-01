1aba221c181da39c53661207058b0660f1f856ba
Elyees Tatua <elyeestatua@gmail.com>
2024-03-01 23:42:42 +0300

    feat(src): resetting up codebase

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -39,6 +39,7 @@ const options = Country.getAllCountries().map(country => ({
 function CityPicker() {
     const [selectedCountry, setSelectedCountry] = useState<option>(null);
     const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+
     const router = useRouter();
 
     const handleSelectedCountry = (option: option) => {
@@ -46,54 +47,44 @@ function CityPicker() {
       setSelectedCity(null);
     };
 
-
     const handleSelectedCity = (option: cityOption) => {
-      `/location/${option?.value.name}/${option?.value.latitude}/${option?.value.longitude}`
-    );
-  };
+      setSelectedCity(option);
+      router.push(
+        
+      )
+    
+    }
 
   return (
-    <div className="space-y-4">
-      <div className="space-y-2">
-        <div className="flex items-center space-x-2 text-white/80">
-          <GlobeIcon className="h-5 w-5 text-white" />
-          <label htmlFor="country">Country</label>
-        </div>
-        <Select
-          className="text-black"
-          value={selectedCountry}
-          onChange={handleSelectedCountry}
-          options={options}
-        />
+  <div className="space-y-4">
+    <div className="space-y-2">
+    <div className="flex items-center space-x-2 text-white/80">
+        <GlobeIcon className="h-5 w-5 text-white font-semibold " />
+        <label htmlFor="country">Country</label>
       </div>
+    <div>
+      <Select 
+      className="text-black font-medium"
+      value={selectedCountry}
+      onChange={handleSelectedCountry}
+      options={options} />
+  </div>
 
-
-  {selectedCountry && (
-        <div className="space-y-2">
-          <div className="flex items-center space-x-2 text-white/80">
-            <GlobeIcon className="h-5 w-5 text-white" />
-            <label htmlFor="country">City</label>
-          </div>
-          <Select
-            className="text-black"
-            value={selectedCity}
-            onChange={handleSelectedCity}
-            options={City.getCitiesOfCountry(
-              selectedCountry.value.isoCode
-            )?.map((state) => ({
-              value: {
-                latitude: state.latitude!,
-                longitude: state.longitude!,
-                countryCode: state.countryCode,
-                name: state.name,
-                stateCode: state.stateCode,
-              },
-              label: state.name,
-            }))}
-          />
-        </div>
-      )}
-    </div>
+  <div className="space-y-2">
+    <div className="flex items-center space-x-2 text-white/80">
+        <GlobeIcon className="h-5 w-5 text-white font-semibold " />
+        <label htmlFor="country">City</label>
+      </div>
+    <div>
+      <Select 
+      className="text-black font-medium"
+      value={selectedCity}
+      onChange={handleSelectedCity}
+      options={options} />
+  </div>  
+  </div>
+  </div>
+</div>
   );
 }
 
