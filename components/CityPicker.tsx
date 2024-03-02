15832d4299b396018aeb805e453c2cfa4b0ba294
Elyees Tatua <elyeestatua@gmail.com>
2024-03-02 22:57:30 +0300

    feat(src): resetting up codebase

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -50,7 +50,7 @@ function CityPicker() {
     const handleSelectedCity = (option: cityOption) => {
       setSelectedCity(option);
       router.push(
-        
+        `/location/${option?.value.name}/${option?.value.latitude}/${option?.value.longitude}`
       )
     
     }
