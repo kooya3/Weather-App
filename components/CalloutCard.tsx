d943130756a01c6243ffc48a6eb59d6644588e44
Elyees Tatua <elyeestatua@gmail.com>
2024-03-10 23:42:47 +0300

    chore(page): Update on the UI

--- components/CalloutCard.tsx
+++ components/CalloutCard.tsx
@@ -10,7 +10,7 @@ function CalloutCard({ message, warning }: Props) {
     className="mt-4"
     title={message}
     icon={warning ? ExclamationIcon : CheckCircleIcon}
-    
+    color={warning ? "rose" : "teal"}
     />
   );
 }
