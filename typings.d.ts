fae2b3daf6d4145ad544ea5a0fb63e456c8ebaac
Elyees Tatua <elyeestatua@gmail.com>
2024-04-01 19:36:51 +0300

    fix(typings): Working on interface values

--- typings.d.ts
+++ typings.d.ts
@@ -2,4 +2,8 @@ interface CurrentWeather {
     is_day: number;
     temperature: number;
     time: string;
-  
+    weathercode: number;
+    winddirection: number;
+    windspeed: number;
+  }
+    
