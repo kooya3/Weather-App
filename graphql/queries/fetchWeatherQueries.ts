1d1b0760c41c82606078ffdc2dd15d483d283b57
Elyees Tatua <elyeestatua@gmail.com>
2024-04-01 22:56:44 +0300

    chore(update): Working on fetch weather query graphql

--- graphql/queries/fetchWeatherQueries.ts
+++ graphql/queries/fetchWeatherQueries.ts
@@ -3,5 +3,6 @@ import { gql } from "@apollo/client";
 const fetchWeatherQuery = gql`
  query MyQuery(
     $current_weather: String
+    $daily: String = "weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max"
   
 `
