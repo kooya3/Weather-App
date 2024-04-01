003ac8f77102c5db0c09aef4ec51c4291aed7a1d
Elyees Tatua <elyeestatua@gmail.com>
2024-04-01 20:21:01 +0300

    chore(apollo): Setting up client cache

--- apollo-client.ts
+++ apollo-client.ts
@@ -5,6 +5,6 @@ let client: ApolloClient<any> | null = null;
 export const getClient = () => {
     const client = new ApolloClient({
         uri: process.env.API_URL,
-        cache:
+        cache: new InMemoryCache(),
 })
 }
