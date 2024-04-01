8752831ec8ac8904b418d360179f4114980cc272
Elyees Tatua <elyeestatua@gmail.com>
2024-04-01 20:12:17 +0300

    chore(dependecy): Added apollo backend client

--- apollo-clint.ts
+++ apollo-clint.ts
@@ -1,3 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 let client: ApolloClient<any> | null = null;
+
+export const getClient = () => {
+    const client = new ApolloClient{
+        
+    }
+}
