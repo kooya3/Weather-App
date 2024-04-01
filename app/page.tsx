dcd6cba66ffbaad14d67cf3f68e047eca06be665
Elyees Tatua <elyeestatua@gmail.com>
2024-04-01 20:37:28 +0300

    chore(app): Switching color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#0e1423] to-[#070a10] p-10 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-br from-[#070a10] to-[#070a10] p-10 flex flex-col justify-center items-center">
       <Card className="max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">
