d116a93e29ba3a4756e4d02ba443ab3a66a9e586
Elyees Tatua <elyeestatua@gmail.com>
2024-04-20 20:10:34 +0300

    refactor(location): update return function

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -36,7 +36,12 @@ function Weatherpage({params: {city, lat, long}}: Props) {
 
     <div>
       <div className="p-5">
-        <div className="pb-5"></div>
+        <div className="pb-5">
+          <h2 className="text-xl font-bold">Todays Overview</h2>
+          <p className="text-sm text-gray-400">
+            
+          </p>
+        </div>
       </div>
     </div>
   </div>);
