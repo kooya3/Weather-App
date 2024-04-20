20365a601ebbbad6e163b46a296f8a215478fce2
Elyees Tatua <elyeestatua@gmail.com>
2024-04-20 22:15:17 +0300

    refactor(TempChart): Defining function

--- components/TempChart.tsx
+++ components/TempChart.tsx
@@ -5,4 +5,12 @@ import { Card, AreaChart, Title } from "@tremor/react";
 type Props = {
     results: Root;
   };
-  
+    
+  function TempChart({ results }: Props) {
+    const hourly = results?.hourly.time
+      .map((time) =>
+        new Date(time).toLocaleString("en-US", {
+          hour: "numeric",
+          hour12: false,
+        })
+      )
