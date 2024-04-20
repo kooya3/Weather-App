df9bf9d2aa558dfdfc9f5b9c627f626f5301ee8d
Elyees Tatua <elyeestatua@gmail.com>
2024-04-20 23:57:00 +0300

    refactor(TempChart): Defining function

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -55,7 +55,10 @@ function Weatherpage({ params: { city, lat, long } }: Props) {
 
       <div className="flex-1 p-5 lg:p-10">
       <div className="p-5">
-          
+      <div className="pb-5">
+            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <p className="text-sm text-gray-400">
+            
             </p>
           </div>
 
