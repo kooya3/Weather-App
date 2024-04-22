9a27f6ef440976160109a785fdd6624f0bcc8758
Elyees Tatua <elyeestatua@gmail.com>
2024-04-22 22:22:25 +0300

    refactor(page): Defining StatCard

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -97,7 +97,7 @@ function Weatherpage({ params: { city, lat, long } }: Props) {
             <div>
               <StatCard
                 title="UV Index"
-                metric={results.daily.
+                metric={results.daily.uv_index_max
 
       </div>
     </div>
