a2d031aac2e386c640e35f7d5b3bb978453b5bd0
Elyees Tatua <elyeestatua@gmail.com>
2024-04-23 16:06:33 +0300

    refactor(loading): location loading functionality

--- app/location/[city]/[lat]/[long]/loading.tsx
+++ app/location/[city]/[lat]/[long]/loading.tsx
@@ -4,3 +4,7 @@ function Loading() {
     return (
         <div className="bg-gradient-to-br from-[#394F68] to-[#183B7E] min-h-screen flex flex-col items-center justify-center text-slate-500">
         <SunIcon
+          className="h-24 w-24 animate-bounce text-yellow-500"
+          color="yellow"
+        />
+  
