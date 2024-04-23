ad05fffc2870e17aaceeda1843a17aa2a5ebc994
Elyees Tatua <elyeestatua@gmail.com>
2024-04-23 16:05:54 +0300

    refactor(loading): location loading classname functionality

--- app/location/[city]/[lat]/[long]/loading.tsx
+++ app/location/[city]/[lat]/[long]/loading.tsx
@@ -2,4 +2,4 @@ import { SunIcon } from "@heroicons/react/solid";
 
 function Loading() {
     return (
-        <div className="bg-gradient-to-br from-[#394F68] to-[#183B7E] 
+        <div className="bg-gradient-to-br from-[#394F68] to-[#183B7E] min-h-screen flex flex-col items-center 
