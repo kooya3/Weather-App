09c22f999c1e593d3a60c3686b35a2596870ff47
Elyees Tatua <elyeestatua@gmail.com>
2024-04-23 16:09:07 +0300

    refactor(loading): location loading functionality

--- app/location/[city]/[lat]/[long]/loading.tsx
+++ app/location/[city]/[lat]/[long]/loading.tsx
@@ -10,6 +10,4 @@ function Loading() {
         <h1 className="text-6xl font-bold text-center mb-10 animate-pulse">
           Loading City Weather Information
         </h1>
- 
-
-  
+        <h2 className="text-xl font-bold text-center mb-10 animate-pulse">
