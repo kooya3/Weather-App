447226aaf6fe7d1954c9bac96e0504d46f3aacdc
Elyees Tatua <elyeestatua@gmail.com>
2024-04-25 23:45:43 +0300

    chore(client): async fn setup

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -11,7 +11,16 @@ type Props = {
 
 async function WeatherPage({ params: { city, lat, long } }: Props) {
   const client = getClient();
-
+  
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: "GMT",
+    },
+  });
   
   
 
