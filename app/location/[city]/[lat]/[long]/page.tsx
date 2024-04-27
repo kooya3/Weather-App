97c02bf86821e88add5c36319f8e50e24a1c3404
Elyees Tatua <elyeestatua@gmail.com>
2024-04-27 23:45:38 +0300

    refactor(query): update

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -29,7 +29,7 @@ async function WeatherPage({ params: { city, lat, long } }:
 
   return (
    <div>
-     Weather{city} {lat} {long} 
+     Weather update{city} {lat} {long} 
    </div>    
     );
 }
